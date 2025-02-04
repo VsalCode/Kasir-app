1be4f559f1c4c699b67ffbaf6e68879971116c64
vsal19 <vsal.ilham@gmail.com>
2025-02-04 23:43:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavbarComponent from './components/Navbar.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavbarComponent />
+    </div>
+  );
+}
+
+export default App;
